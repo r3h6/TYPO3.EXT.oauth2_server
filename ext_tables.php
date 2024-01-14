790970bd3188f5a64a0888c4e20df070474847b6
Marco Huber <mail@marco-huber.de>
2024-01-15 00:00:29 +0100

    Move TCA manipulation to cached file

--- ext_tables.php
+++ ext_tables.php
@@ -1,11 +1,3 @@
 <?php
 defined('TYPO3') || die('Access denied.');
 
-call_user_func(
-    function()
-    {
-
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('oauth2_server', 'Configuration/TypoScript', 'OAuth2 Server');
-
-    }
-);
